1774c20fed4a70be7581f917b78e8097990ad110
chfw <wangc_2011@hotmail.com>
2019-07-27 22:18:32 +0100

    :sparkles: enable multiple fs to be searched, like FileSystemLoader, which allow a single folder or a list of folders

--- jinja2_fsloader/__init__.py
+++ jinja2_fsloader/__init__.py
@@ -7,7 +7,7 @@ import fs.path
 import fs.errors
 import jinja2
 import pkg_resources
-
+from jinja2._compat import string_types
 
 __author__ = "Martin Larralde <martin.larralde@ens-paris-saclay.fr>"
 __license__ = "MIT"
@@ -32,30 +32,38 @@ class FSLoader(jinja2.BaseLoader):
 
     """
 
-    def __init__(self, template_fs, encoding='utf-8', use_syspath=False):
-        self.fs = fs.open_fs(template_fs)
+    def __init__(self, template_fs_list, encoding='utf-8', use_syspath=False):
+        if isinstance(template_fs_list, string_types):
+            template_fs_list = [template_fs_list]
+        self.template_fs_list = template_fs_list
         self.use_syspath = use_syspath
         self.encoding = encoding
 
     def get_source(self, environment, template):
-        if not self.fs.isfile(template):
+        for template_fs in self.template_fs_list:
+            with fs.open_fs(template_fs) as fs_handle:
+                if not fs.isfile(template):
+                    continue
+                try:
+                    mtime = fs.getdetails(template).modified
+                    reload = lambda: fs_handle.getdetails(template).modified > mtime
+                except fs.errors.MissingInfoNamespace:
+                    reload = lambda: True
+                with fs.open(template, encoding=self.encoding) as f:
+                    source = f.read()
+                if self.use_syspath:
+                    if fs.hassyspath(template):
+                        return source, fs_handle.getsyspath(template), reload
+                    elif fs.hasurl(template):
+                        return source, fs_handle.geturl(template), reload
+                return source, template, reload
+        else:
             raise jinja2.TemplateNotFound(template)
-        try:
-            mtime = self.fs.getdetails(template).modified
-            reload = lambda: self.fs.getdetails(template).modified > mtime
-        except fs.errors.MissingInfoNamespace:
-            reload = lambda: True
-        with self.fs.open(template, encoding=self.encoding) as f:
-            source = f.read()
-        if self.use_syspath:
-            if self.fs.hassyspath(template):
-                return source, self.fs.getsyspath(template), reload
-            elif self.fs.hasurl(template):
-                return source, self.fs.geturl(template), reload
-        return source, template, reload
 
     def list_templates(self):
         found = set()
-        for file in self.fs.walk.files():
-            found.add(fs.path.relpath(file))
+        for template_fs in self.template_fs_list:
+            with fs.open_fs(template_fs) as fs_handle:
+                for file in fs_handle.walk.files():
+                    found.add(fs.path.relpath(file))
         return sorted(found)
